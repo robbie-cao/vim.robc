b784144b7f3280be98d7d7c0dae73194c3ccf6c9
Robbie Cao <robbie.cao@gmail.com>
2016-02-14 19:09:33 +0800

    Check YouCompleteMe exist or not before compilie it
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- install.sh
+++ install.sh
@@ -45,8 +45,10 @@ export SHELL=$system_shell
 echo "Step5: compile YouCompleteMe"
 echo "It will take a long time, just be patient!"
 echo "If error,you need to compile it yourself"
-echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && bash -x install.sh --clang-completer"
-cd $CURRENT_DIR/bundle/YouCompleteMe/
+if [ ! -e $CURRENT_DIR/bundle/YouCompleteMe ]; then
+    echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && bash -x install.sh --clang-completer"
+    cd $CURRENT_DIR/bundle/YouCompleteMe/
+fi
 
 if [ `which clang` ]   # check system clang
 then
